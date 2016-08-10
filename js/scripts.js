70e29bdb426f931881c1dd336f94cbacdbe68718
Nicolas Hurtado and Sam Peppard <sam@>
2016-08-10 14:41:50 -0400

    add event handler functions for subtraction, multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -17,11 +19,41 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
+
+  // Addition Event Handler
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var resultAdd = add(number1, number2);
-    $("#output").text(resultAdd);
+    var result = add(number1, number2);
+    $(this).next().text(result);
   });
+
+  // Subtraction Event Handler
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = sub(number1, number2);
+    $(this).next().text(result);
+  });
+
+  // Multiplication Event Handler
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number1, number2);
+    $(this).next().text(result);
+  });
+
+  // Division Event Handler
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $(this).next().text(result);
+  });
+
 });
