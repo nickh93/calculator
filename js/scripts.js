d2398e81b0c966e1b492c481dce5e96a77dcc5a3
Nicolas Hurtado and Sam Peppard <nicolas@>
2016-08-10 11:35:42 -0400

    add user-interface logic for addition

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
+// Everything below this line is user interface (or front-end) logic:
 
-
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var resultAdd = add(number1, number2);
+    $("#output").text(resultAdd);
+  });
+});
